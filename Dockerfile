77879262660f0c0e1cf22ee2a149e46242b8c644
JackNoordhuis <jackjnoordhuis@gmail.com>
2020-01-08 00:10:32 +1100

    work plz

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
 FROM nxtlvlsoftware/pmmp-phpstan:master
 
-VOLUME /source
-
 ADD entrypoint.sh /entrypoint.sh
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-sh -c "sudo chown -R pocketmine /github"
-sh -c "sudo chmod -R 777 /github"
 sh -c "ln -s /github/workspace /source && analyze $*"
