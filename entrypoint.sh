637b676766e892ceeb373e9adc8bedc8e595d726
JackNoordhuis <jackjnoordhuis@gmail.com>
2020-01-08 22:24:25 +1100

    symlink cloned repo to /source

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,8 @@
-#!/bin/sh -l
+#!/bin/sh
 
-sh -c "analyze $*"
+# if path to analyze is provided we symlink it to /source so the default phpstan config works
+if [ ! -z "$1" ]; then
+	sh -c "ln -s $1 /source"
+fi
+
+sh -c "ln -s $1 /source && analyze $*"
