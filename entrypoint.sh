f72bd6ba52400e91cf73059a44c4ed7e23c70398
JackNoordhuis <jackjnoordhuis@gmail.com>
2020-01-08 22:37:26 +1100

    symlink default checkout location if no path is provided

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,8 @@
 # if path to analyze is provided we symlink it to /source so the default phpstan config works
 if [ ! -z "$1" ]; then
 	sh -c "ln -s $1 /source"
+else # if no custom path is specified we symlink the default checkout location to /source
+	sh -c "ln -s /github/workflow /source"
 fi
 
 sh -c "analyze $*"
