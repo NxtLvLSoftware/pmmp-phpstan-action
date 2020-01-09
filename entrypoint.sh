60ab2992eff58e85856113591640887b482498a5
JackNoordhuis <jackjnoordhuis@gmail.com>
2020-01-10 09:45:30 +1100

    fix symlinking custom source dir

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,11 @@
 
 # if path to analyze is provided we symlink it to /source so the default phpstan config works
 if [ ! -z "$1" ]; then
-	sh -c "ln -s $1 /source"
+    echo "linking $PWD/$1 to /source"
+	sh -c "ln -s $PWD/$1 /source"
 else # if no custom path is specified we symlink the default checkout location to /source
-	sh -c "ln -s /github/workflow /source"
+    echo "linking $PWD to /source"
+	sh -c "ln -s $PWD /source"
 fi
 
 sh -c "analyze $*"
